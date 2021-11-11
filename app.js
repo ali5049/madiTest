a908b69d7f625d8eeb70f08dc6921dcccf96eeeb
Mac <alihayder133@gmail.com>
2021-11-12 01:52:06 +0500

    changed port

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
+const port = process.env.PORT || 8081;
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 // Init middleware
 app.use(express.json({ extended: false }));
@@ -18,6 +19,6 @@ app.get("/", function (req, res) {
 
 app.use('/api/calculator',calculatorRoutes );
 
-app.listen(3001, function () {
-  console.log("Example app listening on port 3000!");
+app.listen(port, function () {
+  console.log("listening at port",process.env.PORT)
 });
